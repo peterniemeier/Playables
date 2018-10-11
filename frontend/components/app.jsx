885a06729f84850f9aed4253269f56d49212fb27
Peter <peter.niemeier@gmail.com>
2018-10-10 17:00:23 -0700

    changes

---
+++ frontend/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Route } from 'react-router-dom';
+
+export default () => (
+  <div>
+    hello
+    // <Route path="/" component={NavBarContainer} />
+    // <Route exact path="/" component={Home} />
+    // <Route path="/chirps" component={ChirpIndexContainer} />
+  </div>
+);
